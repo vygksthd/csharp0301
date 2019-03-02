c512c3b95b6e911ee02466a9fda1d0e878ff8cf4
junhong cheon <cjswnsghd12@gmail.com>
2019-03-02 18:20:25 +0900

    Page48_Shapes

---
+++ Page48_Shapes/Page48_Shapes/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page48_Shapes
+{
+    public class Circle
+    {
+        double radius;
+        public Circle()
+        {
+            radius = 0;
+        }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double Area()
+        {
+            return Math.PI * (radius * radius);
+        }
+    }
+}
