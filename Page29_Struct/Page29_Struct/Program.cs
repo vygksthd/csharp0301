c66af66f441a9f6e6f8bca726271298d7f8f57bb
junhong cheon <cjswnsghd12@gmail.com>
2019-03-02 14:48:13 +0900

    Page29_Struct

---
+++ Page29_Struct/Page29_Struct/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page29_Struct
+{
+    struct Emp
+    {
+        public int id;
+        public string name;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp e;
+            e.id = 1;
+            e.name = "홍길동1";
+            Console.WriteLine("사번: {0}", e.id );
+            Console.WriteLine("이름: {0}", e.name);
+
+            Emp e1 = new Emp();
+            e1.id = 2;
+            e1.name = "홍길동2";
+            Console.WriteLine("사번: {0}", e1.id);
+            Console.WriteLine("이름: {0}", e1.name);
+        }
+    }
+}
