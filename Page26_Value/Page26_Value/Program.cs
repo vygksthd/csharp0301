173ba269a76a1ac464ed844ccb7a69d616260eb0
junhong cheon <cjswnsghd12@gmail.com>
2019-03-02 14:11:11 +0900

    Value

---
+++ Page26_Value/Page26_Value/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page26_Value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = a;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            b = 2;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
