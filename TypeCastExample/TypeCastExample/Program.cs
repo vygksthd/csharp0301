6b5c8d3282c239d29050c024adfe1ee21b59f9d9
junhong cheon <cjswnsghd12@gmail.com>
2019-03-02 13:28:30 +0900

    타입 변환

---
+++ TypeCastExample/TypeCastExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace TypeCastExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iSize = sizeof(int);
+            Console.WriteLine("int형 바이트길이 : {0}", iSize);
+
+            Type myType1 = typeof(int);
+            Console.WriteLine("typeof(int) : {0}", myType1);
+
+            int i = 10;
+            Type myType2 = i.GetType();
+            Console.WriteLine("i.GetType() : {0}", myType2);
+
+            int j = 99; //변수의 사이즈 구할 때
+            Console.WriteLine("Size of j : {0}", Marshal.SizeOf(j));
+
+        }
+    }
+}
