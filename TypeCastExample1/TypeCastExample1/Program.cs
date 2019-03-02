374d46d204529d448a22f0fc947801656419cbd8
junhong cheon <cjswnsghd12@gmail.com>
2019-03-02 11:51:09 +0900

    C# 예제

---
+++ TypeCastExample1/TypeCastExample1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCastExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 10;
+            int b = a;
+            int c = 50000;
+            short d = (short)c;
+            Console.WriteLine("Short : {0}", b);
+            Console.WriteLine("Short : {0}", d);
+        }
+    }
+}
