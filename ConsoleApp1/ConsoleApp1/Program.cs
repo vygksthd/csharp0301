3055e385b2bdcbd96ce15294dc67e96e780ef78c
junhong cheon <cjswnsghd12@gmail.com>
2019-03-02 10:31:36 +0900

    hello

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellowWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hellow C#~");
+            System.Console.WriteLine("Hellow C#~");
+            System.Console.WriteLine("Hellow C#~");
+        }
+    }
+}
