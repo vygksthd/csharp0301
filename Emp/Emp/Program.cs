d757a1ca8940bd8951ebd683dadfa85182889d74
junhong cheon <cjswnsghd12@gmail.com>
2019-03-02 18:05:28 +0900

    Page44_속성

---
+++ Emp/Emp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp
+{
+    class Emp
+    {
+        private int _Empno;
+        public int Empno { get; set; } = 998;
+    }
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+            Emp e = new Emp();
+            e.Empno = 999;
+            Console.WriteLine(e.Empno);
+        }
+    }
+}
