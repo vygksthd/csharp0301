5fa1365329f9858354e57ac2457678a45bfd4db3
junhong cheon <cjswnsghd12@gmail.com>
2019-03-02 14:13:36 +0900

    Page27_Reference

---
+++ Page23_Reference/Page23_Reference/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page23_Reference
+{
+    class Rtype
+    {
+        public object Value;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rtype a = new Rtype();
+            Rtype b = new Rtype();
+
+            a.Value = 1;
+            Console.WriteLine(a.Value);
+
+            b.Value = 2;
+            Console.WriteLine(b.Value);
+        }
+    }
+}
