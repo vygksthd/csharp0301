7829e19b9bf70c3abc7ced036493bb31c5ffa9c0
junhong cheon <cjswnsghd12@gmail.com>
2019-03-02 12:12:36 +0900

    메인 두개일 경우

---
+++ MainArgTest/MainArgTest/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainArgTest
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello {0}, {1}", args[0], args[1]);
+        }
+    }
+}
