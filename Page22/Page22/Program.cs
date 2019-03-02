4d940c64d639005449a04a42f5d3b0bb1dca9308
junhong cheon <cjswnsghd12@gmail.com>
2019-03-02 14:04:43 +0900

    조건 연산자

---
+++ Page22/Page22/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object a = null;
+            object b = "abc";
+
+            object c = a ?? b;
+            Console.WriteLine(c);
+
+            object d;
+
+            if(a!=null)
+            {
+                d = a;
+            }
+            else
+            {
+                d = b;
+            }
+            Console.WriteLine(d);
+
+            object e;
+            e = (a != null) ? a : d;
+            Console.WriteLine(e);
+        }
+    }
+}
