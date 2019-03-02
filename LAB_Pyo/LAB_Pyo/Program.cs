a199f83a5b47cb58a5383ae7a1b6f46d9105f4dc
junhong cheon <cjswnsghd12@gmail.com>
2019-03-02 16:11:28 +0900

    입력한 숫자대로 구구단 보여주기

--- LAB_Pyo/LAB_Pyo/Program.cs
+++ LAB_Pyo/LAB_Pyo/Program.cs
@@ -97,7 +97,7 @@ namespace LAB_Pyo
             }
             for (int i = 2; i < 10; i++)
             {
-                for (int j = 1; j < 10; j++)
+                for (int j = 1; j <= m; j++)
                 {
                     Console.Write(j + " x " + i + " = " + i * j + "\t");
                 }
